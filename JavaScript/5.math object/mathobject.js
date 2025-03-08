c5bf5df34a605506fbc7a2f341817d8656c88456
Deep Gandhi <deepgandhi000@gmail.com>
2025-03-08 22:17:46 +0530

    math object

---
+++ JavaScript/5.math object/mathobject.js
@@ -0,0 +1,48 @@
+// What is the output of the following?
+console.log(Math.floor(4.9)); //4
+
+// What is the output of the following?
+console.log(Math.ceil(4.1)); //5
+
+// What is the output of the following?
+console.log(Math.round(6.5)); //7
+console.log(Math.round(6.2)); //6
+
+// What is the output of the following?
+console.log(Math.trunc(3.9)); // 3 (removes the decimal part)
+
+// What is the output of the following?
+console.log(Math.pow(2, 3)); // 8
+
+// What is the output of the following?
+console.log(Math.sqrt(16)); // 4
+
+// What is the output of the following?
+console.log(Math.abs(-5.3)); // 5.3
+
+// What is the output of the following?
+console.log(Math.min(3, 1, 7, 0)); //0
+
+// What is the output of the following?
+console.log(Math.max(3, 1, 7, 0)); //0
+
+// What is the output of the following?
+console.log(Math.random()); //0.14037798719490047 (generate between 0 and 1)
+console.log(Math.floor(Math.random() * 10) + 1);
+
+// What is the output of the following?
+console.log(Math.sign(-7)); // -1
+console.log(Math.sign(7)); // 1
+console.log(Math.sign()); // NaN
+console.log(Math.sign(0)); // 0
+
+// What is the output of the following?
+console.log(Math.cbrt(27)); // 3 (finds cube root)
+
+// What is the output of the following?
+console.log(Math.log(1)); //0
+console.log(Math.log(0)); // -Infinity
+console.log(Math.log(-1)); // NaN
+
+// What is the output of the following?
+console.log(Math.PI.toFixed(2)); //3.14
