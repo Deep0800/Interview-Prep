6cb32da513b7ffdd5a1fe402545e24c8ff4c3e33
Deep Gandhi <deepgandhi000@gmail.com>
2025-03-09 11:06:17 +0530

    operator

---
+++ JavaScript/7.operator/1.codeBase.js
@@ -0,0 +1,46 @@
+// What is the output?
+console.log(5 + "5"); //55
+
+// What is the output?
+console.log(5 - "2"); //3
+
+// What is the output?
+console.log(null == undefined); //true
+console.log(null === undefined); //false
+
+// What is the output?
+console.log(0 || "Hello"); //Hello (|| returns the first truthy value)
+
+// What is the output?
+console.log(1 && 2 && 0 && 3); //0  (&& returns the first falsy value)
+
+// What is the output?
+console.log(!!"false"); //true
+console.log(!!false); //false
+
+// What is the output?
+console.log(5 & 3); //1 (Binary 101 & 011 = 001)
+
+// What is the output?
+console.log(typeof NaN); //number
+
+// What is the output?
+console.log(false ?? "default"); //false (?? only checks for null and undefined)
+
+// What is the output?
+console.log(10 >> 1); //5 (10 >> 1 shifts bits right, equivalent to 10 / 2)
+
+// What is the output?
+console.log(0 ?? "JS"); //  0 (Nullish coalescing ?? only checks null and undefined)
+
+// What is the output?
+console.log("5" - -"5") //10
+
+// What is the output?
+console.log(4 | 3); //7
+
+// What is the output?
+console.log([] + []); //"" (Empty arrays convert to empty strings in concatenation)
+
+// What is the output?
+console.log(1 + true); //2 true is converted to 1, so 1 + 1 = 2)
