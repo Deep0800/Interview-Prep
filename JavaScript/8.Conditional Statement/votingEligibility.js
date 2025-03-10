7f135447c8a45da1055ef59ecf70c36401f6cdf7
Deep Gandhi <deepgandhi000@gmail.com>
2025-03-10 16:29:54 +0530

    conditional statement

---
+++ JavaScript/8.Conditional Statement/votingEligibility.js
@@ -0,0 +1,7 @@
+let age = 16;
+
+if (age >= 18) {
+    console.log("You can vote now")
+}else{
+    console.log(`You are under age After ${18-age} years you can vote`)
+}
---
+++ JavaScript/8.Conditional Statement/vowelOrConsonant.js
@@ -0,0 +1,7 @@
+let char = "f"
+
+if ("aeiou".includes(char)) {
+    console.log(`${char} is Vowel`)
+}else{
+    console.log(`${char} is Consonant`)
+}
