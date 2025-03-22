0a07de0c1fcd6859f3ba9324e7f22d88969bb67d
Deep Gandhi <deepgandhi000@gmail.com>
2025-03-22 10:55:45 +0530

    programs

--- JavaScript/7.operator/1.codeBase.js
+++ JavaScript/7.operator/1.codeBase.js
@@ -34,7 +34,7 @@ console.log(10 >> 1); //5 (10 >> 1 shifts bits right, equivalent to 10 / 2)
 console.log(0 ?? "JS"); //  0 (Nullish coalescing ?? only checks null and undefined)
 
 // What is the output?
-console.log("5" - -"5") //10
+console.log("5" - -"5"); //10
 
 // What is the output?
 console.log(4 | 3); //7
@@ -44,3 +44,18 @@ console.log([] + []); //"" (Empty arrays convert to empty strings in concatenati
 
 // What is the output?
 console.log(1 + true); //2 true is converted to 1, so 1 + 1 = 2)
+
+// What is the output?
+let num = 5 + "5" - 5;
+console.log(num); //50
+console.log(typeof num); //number
+
+// What is the output?
+let num1 = 10;
+let num2 = 20;
+console.log("Sum of num1 and num2=" + num1 + num2); //Sum of num1 and num2=1020
+console.log("Sum of num1 and num2=", num1 + num2); //Sum of num1 and num2= 30
+
+// What is the output?
+console.log("5" * 5); //25
+console.log("25" / 5); //5
---
+++ JavaScript/7.operator/ternary.js
@@ -0,0 +1,8 @@
+// ternary operator ? :
+// condition ? true part : false part
+
+console.log(112 > 13 ? true : false);
+
+// nested ternary operator
+let num = 0;
+console.log(num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero");
